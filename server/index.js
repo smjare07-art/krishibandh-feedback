3df0756e93b5a1501b5330e172b4e9012287c7bd
Shubham Jare <smjare07@gmail.com>
2026-04-02 23:08:48 +0530

    MongoDB Atlas added

--- server/index.js
+++ server/index.js
@@ -6,27 +6,33 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// MongoDB connect
-mongoose.connect("mongodb://127.0.0.1:27017/feedbackDB");
+// MongoDB Atlas connect
+mongoose.connect("mongodb+srv://admin:Krishi@123@cluster0.fzzm2wu.mongodb.net/feedbackDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // Schema
 const Feedback = mongoose.model("Feedback", {
   name: String,
   occupation: String,
-  rating: String,
+  rating: Number,
   feedback: String
 });
 
-// API
+// POST API
 app.post("/api/feedback", async (req, res) => {
   const data = new Feedback(req.body);
   await data.save();
   res.send("Saved");
 });
-// GET all feedback
+
+// GET API
 app.get("/api/feedback", async (req, res) => {
   const data = await Feedback.find().sort({ _id: -1 });
   res.json(data);
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+// PORT FIX
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running"));
